31ece01028dc00bb19aab33ea9064a3467e4947d
ihkap11 <bpakhi97@gmail.com>
2023-11-01 18:40:25 +0000

    Refactor: createBackgroundLayers via loop

--- game/background.js
+++ game/background.js
@@ -8,10 +8,12 @@ class Layer {
     this.x = 0;
     this.y = 0;
   }
+
   update() {
     if (this.x < -this.width) this.x = 0;
     else this.x -= this.game.speed * this.speedModifier;
   }
+
   draw(context) {
     context.drawImage(this.image, this.x, this.y, this.width, this.height);
     context.drawImage(
@@ -30,61 +32,35 @@ export class Background {
     this.width = 1667;
     this.height = 500;
 
-    this.layer1image = document.getElementById("layer1");
-    this.layer2image = document.getElementById("layer2");
-    this.layer3image = document.getElementById("layer3");
-    this.layer4image = document.getElementById("layer4");
-    this.layer5image = document.getElementById("layer5");
-
-    this.layer1 = new Layer(
-      this.game,
-      this.width,
-      this.height,
-      0,
-      this.layer5image
-    );
-    this.layer2 = new Layer(
-      this.game,
-      this.width,
-      this.height,
-      0.2,
-      this.layer2image
-    );
-    this.layer3 = new Layer(
-      this.game,
-      this.width,
-      this.height,
-      0.4,
-      this.layer3image
-    );
-    this.layer4 = new Layer(
-      this.game,
-      this.width,
-      this.height,
-      0.8,
-      this.layer4image
-    );
-    this.layer5 = new Layer(
-      this.game,
-      this.width,
-      this.height,
-      1,
-      this.layer5image
-    );
-
-    this.backgroundLayers = [
-      this.layer1,
-      this.layer2,
-      this.layer3,
-      this.layer4,
-      this.layer5,
+    this.layerImages = [
+      document.getElementById("layer1"),
+      document.getElementById("layer2"),
+      document.getElementById("layer3"),
+      document.getElementById("layer4"),
+      document.getElementById("layer5"),
     ];
+    this.backgroundLayers = this.createBackgroundLayers();
   }
+
+  createBackgroundLayers() {
+    const layerSpeedModifiers = [0, 0.2, 0.4, 0.6, 0.8, 1.0];
+    return this.layerImages.map((image, index) => {
+      return new Layer(
+        this.game,
+        this.width,
+        this.height,
+        layerSpeedModifiers[index],
+        image
+      );
+    });
+  }
+
   update() {
     this.backgroundLayers.forEach((layer) => {
       layer.update();
     });
   }
+
   draw(context) {
     this.backgroundLayers.forEach((layer) => {
       layer.draw(context);

fd66899b7c9714577f527e0780c9b3db15f5351b
ihkap11 <bpakhi97@gmail.com>
2023-11-02 01:33:55 +0000

    Add: enemies to the  game (enemy class)

--- game/player.js
+++ game/player.js
@@ -3,25 +3,25 @@ import { Sitting, Running, Jumping, Falling } from "./playerStates.js";
 export class Player {
   constructor(game) {
     this.game = game;
+
+    this.image = document.getElementById("player");
     this.width = 100;
     this.height = 91.3;
-
     this.x = 0;
     this.y = this.game.height - this.height - this.game.groundMargin;
     this.vy = 0;
     this.weight = 1;
+    this.speed = 0;
+    this.maxSpeed = 10;
 
-    this.image = document.getElementById("player");
     this.frameX = 0;
     this.frameY = 0;
 
+    // control sprite fps rate, the sheet we are using is made for lower fps
     this.maxFrame;
     this.fps = 20;
     this.frameInterval = 1000 / this.fps;
     this.frameTimer = 0;
-
-    this.speed = 0;
-    this.maxSpeed = 10;
 
     this.states = [
       new Sitting(this),
